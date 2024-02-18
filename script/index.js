7f06a0c33c13e7bb408836f5bc8ff944a5fea75c
saagor16 <saagor.16@gmail.com>
2024-02-18 11:07:45 +0600

    bg- chng

---
+++ script/index.js
@@ -0,0 +1,5 @@
+// scrolling
+document.getElementById('first-btn').addEventListener('click', function() {
+    document.getElementById('main-section').scrollIntoView({ behavior: 'smooth' });
+  });
+  
